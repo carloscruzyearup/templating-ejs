007aa5629bfe91922097a0374102af19c257ae16
c373 <76661778+c373@users.noreply.github.com>
2022-08-16 20:28:19 -0700

    final version

--- index.js
+++ index.js
@@ -4,11 +4,28 @@ const port = 3000
 
 app.set('view engine', 'ejs')
 
+//this will allow us to serve up static files, CSS, images & JS
+app.use(express.static(__dirname));
+
+
 app.get('/', (req, res) => {
-  var title = 'Our Home Page'
-  res.render('index', {'title':title})
+	let title = 'Our Home Page'
+	let heading = 'My Website'
+	res.render('pages/index', {
+		'title': title,
+		'heading': heading
+	})
+})
+
+app.get('/about', (req, res) => {
+	let title = 'Our About Page'
+	let heading = 'My Website'
+	res.render('pages/about', {
+		'title': title,
+		'heading': heading
+	})
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+	console.log(`Example app listening on port ${port}`)
 })
